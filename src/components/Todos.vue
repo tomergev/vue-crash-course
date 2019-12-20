09238c300370f204e1fdc1e49d66c2e304ce7f9b
tomergev <tomergev21@gmail.com>
2019-12-20 13:01:31 -0800

    Initial Commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="todo in todos" v-bind:key="todo.id">
+      <TodoItem v-bind:todo="todo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem'
+
+export default {
+	name: "Todos",
+	components: {
+		TodoItem,
+	},
+  props: ["todos"]
+};
+</script>
+
+<style scoped>
+</style>
